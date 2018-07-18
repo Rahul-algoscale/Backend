4f90c662fe7bebc3a28cfc23909a9c6bf3f79d28
Rahul Kumar Mandal <rahul.mandal@algoscale.com>
2018-07-18 19:59:08 +0530

    checkpoint

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 
 var messages = [{text:'Some text', owner: 'Tim'}, {text: 'other message', owner: 'Jane'}]
+app.use((req, res, next) =>  {
+  res.header("Acess-Control-Allow-Origin", "*");
+  res.header("Acess-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 
 app.get('/messages', (req, res) => {
   res.json(messages);
